6394264ecc273d9c26eca20cc198cdfebbd0dcf8
akashk99 <38279340+akashk99@users.noreply.github.com>
2020-04-10 16:44:16 -0700

    Update sketch.js

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -1,46 +1,28 @@
 var socket;
 
-var playerColor = {}
-
 function setup() {
     createCanvas(800, 700)
     background(50)
     socket = io()
-    socket.on("playerData", drawOthers)
-
-    playerColor = {
-        r: random(255),
-        g: random(255),
-        b: random(255)
-    }
-    
+    socket.on("mouse", drawOthers)
 }
 
-
-function drawOthers(playerData) {
+function drawOthers(mouseData) {
     noStroke();
-    fill(playerData.color.r,playerData.color.g,playerData.color.b)
-    ellipse(playerData.mouseX, playerData.mouseY, 36, 36)
+    fill(255)
+    ellipse(mouseData.x, mouseData.y, 36, 36)
 }
 
-function mouseClicked() {
-    playerColor = {
-        r: random(255),
-        g: random(255),
-        b: random(255)
-    }
-}
+
 
 function mouseDragged() {
-    let playerData = {
-        mouseX,
-        mouseY,
-        color: playerColor
+    let data = {
+        x: mouseX,
+        y: mouseY
     }
-
-    socket.emit("playerData", playerData)
+    socket.emit("mouse", data)
     noStroke();
-    fill(playerColor.r, playerColor.g, playerColor.b)
+    fill(255)
     ellipse(mouseX, mouseY, 36, 36)
 }
 
